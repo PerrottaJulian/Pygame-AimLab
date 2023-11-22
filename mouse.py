f0ffc38b488575e1de6a7626a05d59bc88e0085e
PerrottaJulian <111912746+PerrottaJulian@users.noreply.github.com>
2023-11-22 11:38:28 -0300

    22/11/2023 (2)

--- mouse.py
+++ mouse.py
@@ -1,12 +1,10 @@
 import pygame, sys, random
-pygame.init()
 
-#Pantalla
+
+
+
+#* Datos previos
 size = (1000, 500)
-screen = pygame.display.set_mode(size)
-pygame.mouse.set_visible(0)
-#FPS´s
-clock = pygame.time.Clock()
 #colores
 black = (0,0,0)
 white = (255,255,255)
@@ -16,15 +14,42 @@ green = (0,255,0)
 grey = (50,50,50)
 dark_blue = (0, 0, 15)
 
-def Mira():
-    borde = pygame.draw.circle(screen, red, mouse_pos, 20, 5)
-    centro = pygame.draw.circle(screen, red, mouse_pos, 5)
+#*Pygame
+pygame.init()
+
+screen = pygame.display.set_mode(size)
+pygame.mouse.set_visible(0)
+clock = pygame.time.Clock()
+
+#Sprites
+class Meteoro(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.image.load("meteor.png").convert()
+        self.image.set_colorkey(black)
+        self.rect = self.image.get_rect()
+        
+# Mira
+def Mira(center):
+    borde = pygame.draw.circle(screen, red, center, 20, 5)
+    centro = pygame.draw.circle(screen, red, center, 5)
     
-#
+#meteoritos
+lista_meteoros = pygame.sprite.Group()
+lista_sprites = pygame.sprite.Group()
 
-
+def generarMeteoros():
+    for i in range(25):
+        meteoro = Meteoro()
+        meteoro.rect.x = random.randint(10,990)
+        meteoro.rect.y = random.randint(10,490)
+        lista_meteoros.add(meteoro)
+        lista_sprites.add(meteoro)
+        
+      
 #Bucle principal  
 done = False
+generarMeteoros()
 while not done:
     for event in pygame.event.get():
         #print(event)
@@ -32,14 +57,15 @@ while not done:
             done = True   
         if event.type == pygame.MOUSEBUTTONDOWN:
             print("PEW PEW")
-            pygame.MOUSEBUTTONUP   
+            pygame.MOUSEBUTTONUP 
+    #!Dibujos de pantalla  
     screen.fill(white)
-    
-    #!animacion mouse
+    lista_meteoros.draw(screen)
+    #!Mouse
     mouse_pos = pygame.mouse.get_pos()
     
-    #!dibujo cuadrado
-    Mira()
+    #!dibujo mirilla
+    Mira(mouse_pos)
     
         
     
